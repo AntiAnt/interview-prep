9611d8f6d2bb15a55f6e9a1f8bc8e4e6480a9fe5
Michael Welsh <welsh.11311@gmail.com>
2024-03-28 10:41:59 -0400

    added 2 pointer solution.

--- 2-remove-values-array/test_remove_values.py
+++ 2-remove-values-array/test_remove_values.py
@@ -1,4 +1,23 @@
 from solutions import IterativeSolution, ImprovedIterativeSolution
+from typing import List
+
+"""Solutions"""
+
+
+def two_pointer_method(nums: List[int], val: int) -> int:
+    i = 0
+    n = len(nums)
+
+    while i < n:
+        if nums[i] == val:
+            nums[i] = nums[n - 1]
+            n -= 1
+        else:
+            i += 1
+    return n
+
+
+"""TESTS"""
 
 
 def test_iterative_remove_elements_equal_to_value():
@@ -30,3 +49,23 @@ def test_improved_iterative_remove_elements_equal_to_value():
         expected = test["expected"]
         print(tests)
         assert num_not_value == expected
+
+
+def test_2_pointer_solution():
+    tests = [
+        {"nums": [3, 2, 2, 3], "val": 3, "expected": 2, "expected_array": [2, 2]},
+        {
+            "nums": [0, 1, 2, 2, 3, 0, 4, 2],
+            "val": 2,
+            "expected": 5,
+            "expected_array": [0, 1, 3, 0, 4],
+        },
+    ]
+
+    for test in tests:
+        kwargs = {
+            k: v for k, v in test.items() if k not in ["expected", "expected_array"]
+        }
+        num_not_value = two_pointer_method(**kwargs)
+        nums_left = test["expected"]
+        assert num_not_value == nums_left
