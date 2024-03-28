c82906edcaf3d41cc8ca803529113028f705ea2c
Michael Welsh <welsh.11311@gmail.com>
2024-03-28 12:42:41 -0400

    cleaned up prints

--- 4-shoots-and -laddes/test_solutions.py
+++ 4-shoots-and -laddes/test_solutions.py
@@ -20,49 +20,47 @@ def flip_coin() -> None:
 def play_game(ladders: Dict[int, int], snakes: Dict[int, int], traps: List[int]) -> int:
     player_position = 1
     turns = 0
+    game_won = False
 
     while player_position < 100:
         spaces_to_move = flip_coin()
         potential_next_pos = spaces_to_move + player_position
-        print(f"Player rolled: {spaces_to_move}")
 
         if potential_next_pos >= 100:
-            print("Player wins!!")
+            game_won = True
             break
         elif potential_next_pos in traps:
-            print(f"Player landed on {potential_next_pos}. It's a trap!\nGame Over.")
             break
         elif potential_next_pos in snakes:
             player_position = snakes.get(potential_next_pos)
-            print(
-                f"Player landed on the head of a snake. Sliding down to {player_position}!"
-            )
+
         elif player_position + spaces_to_move in ladders:
             player_position = ladders.get(potential_next_pos)
-            print(
-                f"Player landed at the bottom of a ladder. climbing up to {player_position}!"
-            )
+
         else:
             player_position += spaces_to_move
-            print(f"Player landed on {player_position}.")
 
         turns += 1
 
-    return turns
+    return turns, game_won
 
 
 def game_mean_time(
     num_games: int, ladders: Dict[int, int], snakes: Dict[int, int], traps: List[int]
 ) -> float:
     tot_rounds = 0
+    games_won = 0
     for _ in range(num_games):
-        tot_rounds += play_game(ladders=ladders, snakes=snakes, traps=traps)
+        game_rounds, game_won = play_game(ladders=ladders, snakes=snakes, traps=traps)
+        tot_rounds += game_rounds
+        if game_won:
+            games_won += 1
 
-    return tot_rounds / num_games
+    return (tot_rounds / num_games, games_won)
 
 
 if __name__ == "__main__":
-    mean_rounds = game_mean_time(
+    mean_rounds, games_won = game_mean_time(
         num_games=num_games, ladders=ladders, snakes=snakes, traps=traps
     )
-    print(f"Mean number of rounds: {mean_rounds}")
+    print(f"Mean number of rounds: {mean_rounds} and games won: {games_won}")
